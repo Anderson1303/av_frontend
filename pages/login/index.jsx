a5744dea1b8692b8bc94e16e65ac203677eb5256
unknown <andersonfranz13@gmail.com>
2023-10-11 15:09:37 -0300

    ajusted login layout

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,13 +1,17 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import {FormTextField} from '../../components/Forms/FormTextField';
 import { userService } from 'services';
 import { Button, Box } from '@mui/material';
 import * as Yup from 'yup';
+import { enqueueSnackbar } from "notistack";
+import 'semantic-ui-css/semantic.min.css'
 
-const Login = () => {
+const App = () => {
+
     const router = useRouter();
 
     useEffect(() => {
@@ -32,38 +36,40 @@ const Login = () => {
                 router.push(returnUrl);
             })
             .catch(error => {
+                enqueueSnackbar(
+                    `${error}`,
+                    { variant: "error" }
+                );
                 setError('apiError', { message: error.message || error });
             });
     }
 
-    return (
-        <div className="col-md-6 offset-md-3 mt-5">
-            <div className="card">
-                <Box xs={12}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Box sx={{pl: 100,pt: 50}}>
-                            <Box sx={{pt: 2}} className="form-group">
-                                <label>Usuário: </label> <br/>
-                                <FormTextField inputProps={{name: 'username'}} register={register} control={control} name='username'{...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`}/>
-                            </Box>
-                            <Box sx={{pt: 2}}  className="form-group">
-                                <label>Senha: </label> <br/>
-                                <FormTextField inputProps={{name: 'password'}} register={register} control={control} name='password' {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`}/>
-                            </Box>
-                            <Box sx={{pt: 2, pl: 10}}  className="form-group">
-                                <Button type='submit' disabled={formState.isSubmitting} className="btn btn-primary">{formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
-                                    Entrar
-                                </Button>
-                            </Box>
-                            {errors.apiError &&
-                                <div className="alert alert-danger mt-3 mb-0">{errors.apiError?.message}</div>
-                            }
-                        </Box>
-                    </form>
-                </Box>
-            </div>
+    return <div className="auth-background ui center aligned middle aligned grid">
+        <div class="ui card column auth-form">
+  <div class="content">
+    <div class="header left aligned">Login</div>
+    <div class="description">
+       <form onSubmit={handleSubmit(onSubmit)} class="ui form">
+       <div class="field">
+            <Box className="form-group ui right corner labeled input">
+                <div class="ui label label right corner"><i aria-hidden="true" class="user icon"></i></div>
+                <input type="text" placeholder="Usuário" name='username' {...register('username')}/>
+            </Box>
+            <Box sx={{mt:1}} className="form-group ui right corner labeled input">
+                <div class="ui label label right corner"><i aria-hidden="true" class="asterisk icon"></i></div>
+                <input type="password" placeholder="Senha" name='password' {...register('password')}/>            
+            </Box>
         </div>
-    );
+            <Box className="form-group ui right corner labeled input">
+                <Button type='submit' disabled={formState.isSubmitting} className="btn btn-primary">{formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                Entrar
+                </Button>
+            </Box>
+    </form>
+    </div>
+  </div>
+</div>
+      </div>
 }
 
-export default Login;
+export default App;
