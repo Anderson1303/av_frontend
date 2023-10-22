5203105b2c02b20ff4e0e31fe29b0e31f9218dd8
unknown <andersonfranz13@gmail.com>
2023-10-22 18:37:26 -0300

    ajusted birth notification

--- pages/api/events/type/read.js
+++ pages/api/events/type/read.js
@@ -1,7 +1,14 @@
 import conn from '../../db';
 import { apiHandler } from 'helpers/api';
 
-const getData = async (typeEvent) => {
+const getData = async () => {
+    const query = `select tyev.id, tyev.name_program from type_events tyev`;
+    return await conn.query(
+        query
+    );
+}
+
+const getDataFilter = async (typeEvent) => {
     const query = `select tyev.id, tyev.name_program from type_events tyev
         where tyev.name = $1`;
     const values = [typeEvent]
@@ -10,10 +17,14 @@ const getData = async (typeEvent) => {
     );
 }
 
-
 const handler = async (req, res) => {
-    const {typeEvent} = req.body;
-    const result = await getData(typeEvent);
+    var result = null;
+    if(req.method=="POST"){
+        const {typeEvent} = req.body;
+        result = await getDataFilter(typeEvent);
+    }else{
+        result = await getData();
+    }
     return res.status(200).json({
         data: result.rows,
         success: true,
