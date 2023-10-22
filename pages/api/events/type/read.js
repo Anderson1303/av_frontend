346b53ed7270ff108e7f26b396a67cf772489351
unknown <andersonfranz13@gmail.com>
2023-10-22 18:12:49 -0300

    Ok

--- pages/api/events/type/read.js
+++ pages/api/events/type/read.js
@@ -1,15 +1,19 @@
 import conn from '../../db';
 import { apiHandler } from 'helpers/api';
 
-const getData = async () => {
-    const query = `select tyev.id, tyev.name_program from type_events tyev`;
+const getData = async (typeEvent) => {
+    const query = `select tyev.id, tyev.name_program from type_events tyev
+        where tyev.name = $1`;
+    const values = [typeEvent]
     return await conn.query(
-        query
+        query,values
     );
 }
 
+
 const handler = async (req, res) => {
-    const result = await getData();
+    const {typeEvent} = req.body;
+    const result = await getData(typeEvent);
     return res.status(200).json({
         data: result.rows,
         success: true,
